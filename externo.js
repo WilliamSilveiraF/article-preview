b440d30a874569791d110c38c290e50de5d0f0ee
williamsilveiraf <williamsilveiraf@hotmail.com>
2021-08-25 17:59:08 -0300

    js upload

---
+++ externo.js
@@ -0,0 +1,8 @@
+/*abra o dropup*/
+function myFunction() {
+    document.getElementById("myShareup").classList.toggle("show");
+}
+/*Tela menor 1000px background do ".shareup" virar azul escuro*/
+function myBackground() {
+    document.getElementById("myShareupback").classList.toggle("show2");
+}
